dacbbb8a39c49cadfe2b59c437e7469ad9527adb
chrisKocik69 <12farant636@BA-1432-DT-06.bville.org>
2015-04-24 10:28:39 -0400

    added Heyo

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ print "Say your name, and something cool about yourself!"
 # each person should put a print statement for themselves
 # for example: print "Hi, I'm John, and I like pie."
 
+print "Heyo, im Chris !"
+print "something cool!"
